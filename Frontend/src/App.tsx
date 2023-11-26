bdfd58a10df56480b6ad83ea762645e6c1ad1327
Stian Haugland <stian.hau@hotmail.no>
2023-11-26 16:33:35 +0100

    Add auth on frontend

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,45 +1,59 @@
-import { useQuery } from '@tanstack/react-query'
-import './App.css'
-import { columns } from "./components/columns"
-import { DataTable } from './components/data-table'
-import client from './api'
-import { AddVirksomhetDialog } from './components/AddVirksomhetDialog'
+import { useQuery } from "@tanstack/react-query";
+import "./App.css";
+import { columns } from "./components/columns";
+import { DataTable } from "./components/data-table";
+import client from "./api";
+import { AddVirksomhetDialog } from "./components/AddVirksomhetDialog";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+import LogoutButton from "./components/LogoutButton";
 function App() {
+  const { getAccessTokenSilently } = useAuth0();
 
   const getVirksomheter = async () => {
-    return  await client.GET('/Virksomheter')
-  }
+    // const token = await getAccessTokenSilently()
+    // console.log(token)
+    return await client.GET("/Virksomheter", {
+      // headers: {
+      //   Authorization: `Bearer ${token}`,
+      // },
+    });
+  };
 
   const { data: res, isLoading } = useQuery({
     queryKey: ["virksomheter"],
     queryFn: async () => {
-      return await getVirksomheter()
+      return await getVirksomheter();
     },
   });
 
+
   const table = () => {
     if (isLoading) {
-      return <div>Loading...</div>
+      return <div>Loading...</div>;
     }
-    if(!res?.data){
-      return <div>No data</div>
+    if (!res?.data) {
+      return <div>No data</div>;
     }
 
     return (
-      <div className='flex flex-col gap-2'>
+      <div className="flex flex-col gap-2">
+        <div className="flex justify-end">
+          <LogoutButton />
+        </div>
         <DataTable columns={columns} data={res.data} />
-        <div className='flex justify-center'>
+        <div className="flex justify-center">
           <AddVirksomhetDialog />
         </div>
       </div>
-    )
-  }
+    );
+  };
 
-  return (
-    <div className='max-w-7xl mx-auto p-2'>
-      {table()}
-    </div>
-  )
+  return <div className="max-w-7xl mx-auto p-2">{table()}</div>;
 }
 
-export default App
+// eslint-disable-next-line react-refresh/only-export-components
+export default withAuthenticationRequired(App, {
+  onRedirecting: () => <div>Redirecting you to the login...</div>
+});
+
+// export default App
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,13 +3,26 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const queryClient = new QueryClient();
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Auth0Provider
+        domain="virksomheter.eu.auth0.com"
+        clientId="DY8AS2nEvbRy5JUhm66sxDoTlAUuAMRE"
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+        useRefreshTokens= {true}
+        cacheLocation="localstorage"
+      >
+  
+        <App />
+      </Auth0Provider>
     </QueryClientProvider>
   </React.StrictMode>
 );
